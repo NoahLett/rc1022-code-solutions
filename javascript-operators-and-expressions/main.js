c85c67bb2028fae33f40d3a38605fa2bfb3a1db4
Noah Lett <noahblett96@gmail.com>
2022-10-17 18:26:54 +0000

    javascript-operators-and-expressions solution - Noah Lett

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 10;
+var height = 15;
+var area = width * height;
+console.log('Value of area:', area);
+console.log('Typeof area:', typeof area);
+
+var bill = 10;
+var payment = 15;
+var change = payment - bill;
+console.log('Value of change:', change);
+console.log('Typeof change', typeof change);
+
+var quizzes = 5;
+var midterm = 10;
+var final = 15;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Value of grade', grade);
+console.log('Typeof grade:', typeof grade);
+
+var firstName = 'Noah';
+var lastName = 'Lett';
+var fullName = firstName + ' ' + lastName;
+console.log('Value of fullName:', fullName);
+console.log('Typeof fullName:', typeof fullName);
+
+var pH = 2;
+var isAcidic = pH < 7;
+console.log('Value of isAcidic:', isAcidic);
+console.log('Typeof isAcidic:', typeof isAcidic);
+
+var headCount = 20;
+var isSparta = headCount === 300;
+console.log('Value of isSparta:', isSparta);
+console.log('Typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('Value of motto:', motto);
+console.log('Typeof motto:', typeof motto);
