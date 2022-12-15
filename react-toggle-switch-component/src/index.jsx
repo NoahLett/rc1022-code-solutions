c9f68a57da10943777d7535991f47cc31cda2fc6
Noah Lett <noahblett96@gmail.com>
2022-12-15 18:40:23 +0000

    Completed react-toggle-switch-component challenge - Noah Lett

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,7 @@
+import element from './toggle-switch';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(element);
