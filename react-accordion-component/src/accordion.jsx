e60bdb02ab4d882b1ff2ffcbeaa0121db0727bbe
Noah Lett <noahblett96@gmail.com>
2022-12-19 22:43:36 +0000

    Updated topics array to be incorporated into the component on props

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.topics = [
+    this.props.topics = [
       { title: 'Hypertext Markup Language', text: 'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.' },
       { title: 'Cascading Style Sheets', text: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.' },
       { title: 'JavaScript', text: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.' }
@@ -25,7 +25,7 @@ class Accordion extends React.Component {
 
   render() {
     return (
-      this.topics.map((topic, index) => {
+      this.props.topics.map((topic, index) => {
         const handleClick = () => {
           this.setOpenTopic(index);
         };
